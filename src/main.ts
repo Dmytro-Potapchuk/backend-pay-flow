741ccd0f391e4afcc1385892875b8a871d52d4ca
Dmytro Potapchuk <potapd111_aehit@students.vizja.pl>
2026-03-19 18:59:52 +0100

    fix docker compose

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const logger = new Logger('Bootstrap')
 
-  // Включаем поддержку CORS:
-  app.enableCors()
+  app.enableCors({
+    origin: "*",
+  });
 
-  // Устанавливаем глобальную валидацию:
   app.useGlobalPipes(new ValidationPipe())
 
-  // Конфигурация Swagger:
   const swaggerConfig = new DocumentBuilder()
       .setTitle('PayFlow API')
       .setDescription('Backend PayFlow')
@@ -24,22 +23,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('api', app, document)
 
-  // Установка порта приложения:
-
-
   const PORT = process.env.PORT || 3000
 
-  app.enableCors({
-    origin: "*",
-  });
-
-
-  // Запуск приложения:
-  await app.listen(3000, "0.0.0.0");
+  await app.listen(PORT, "0.0.0.0")
 
   const serverUrl = `http://localhost:${PORT}`
 
-  // Логирование успешного запуска приложения:
   logger.log(`🚀 Server running: ${serverUrl}`)
   logger.log(`📚 Swagger docs: ${serverUrl}/api`)
 }
